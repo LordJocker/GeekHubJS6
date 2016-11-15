e7f96d3307996c42e2c8749de440a75ff08151b3
Vladislav Galushko <lordjocker6300@gmail.com>
2016-11-15 06:32:48 +0200

    Added task 1 to homework 5

---
+++ lesson_5/task_1/script.js
@@ -0,0 +1,79 @@
+var mode = 'Add',
+    el;
+
+
+function addItem() {
+    var input = document.getElementsByName('title')[0];
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    if (input.value) {
+        if (mode == 'Add') {
+            var li = document.createElement('li');
+            var ul = document.getElementById('list');
+
+            input.placeholder = "Type something…";
+            li.innerText = input.value;
+            li.onclick = clicked;
+            input.value = '';
+
+            li.appendChild(checkbox);
+            ul.appendChild(li);
+
+        } else if (mode == 'Edit') {
+            el.innerText = input.value;
+            el.appendChild(checkbox);
+            input.value = '';
+            el.classList.remove('edit');
+            mode = 'Add';
+            document.getElementById('dynamicButton').innerText = mode;
+        }
+
+    }
+}
+
+function removeItem() {
+    el.parentNode.removeChild(el);
+
+    var input = document.getElementsByName('title')[0];
+    input.value = '';
+    mode = 'Add';
+    document.getElementById('dynamicButton').innerText = mode;
+}
+
+function clicked() {
+    el = event.toElement;
+    [].forEach.call(el.parentElement.children, function (el) {
+        el.id = ('');
+        el.classList.remove('edit');
+    });
+    el.id = ('remove');
+    el.classList.add('edit');
+
+    var input = document.getElementsByName('title')[0];
+
+    input.value = el.innerText;
+    mode = 'Edit';
+    document.getElementById('dynamicButton').innerText = mode;
+}
+
+function archiveItem() {
+    var li = document.createElement('li');
+    var ul = document.getElementById('archivedList');
+    var input = document.getElementsByName('title')[0];
+
+    li.innerText = input.value;
+    ul.appendChild(li);
+    removeItem();
+}
+
+function showArchive() {
+    if (document.getElementById('archivedList').style.display == "none") {
+        document.getElementById('archivedList').style.display = "block";
+        document.getElementById('showButton').innerText = 'Close archive';
+    }
+    else {
+        document.getElementById('archivedList').style.display = "none";
+        document.getElementById('showArchive').innerText = 'Show archive';
+    }
+}
