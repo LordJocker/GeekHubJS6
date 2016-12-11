5af957e8663f1571fe14d5d3314e7d9d2d265259
Vladislav Galushko <lordjocker6300@gmail.com>
2016-12-11 11:59:02 +0200

    hw_8 v0.3

--- lesson_8/script.js
+++ lesson_8/script.js
@@ -14,7 +14,6 @@ $(document).ready(function() {
         var i, arr = [], res = [];
         for (i = 1; i <= 15; i++ ) arr.push(i);
         for (i = 0; i < 15; i++) res.push(arr.splice(Math.floor(Math.random() * (arr.length)), 1)[0])
-        console.log(res);
         return res;
     }
 
