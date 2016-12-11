10266823848433b5e0e2d86f461a214e2911e85b
Vladislav Galushko <lordjocker6300@gmail.com>
2016-12-11 11:27:48 +0200

    hw_8 v0.2

---
+++ lesson_8/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $('#btn').click(function newGame() {
+        var arr = randomize();
+        for (var i = 0; i <= 15; i++) {
+            $('#t' + i + '').text(arr[i]);
+        }
+    });
+
+    ()
+
+
+
+    function randomize() {
+        var i, arr = [], res = [];
+        for (i = 1; i <= 15; i++ ) arr.push(i);
+        for (i = 0; i < 15; i++) res.push(arr.splice(Math.floor(Math.random() * (arr.length)), 1)[0])
+        console.log(res);
+        return res;
+    }
+
+});
+
